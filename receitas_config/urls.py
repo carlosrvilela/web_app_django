696cdf5a8ae3411b0f9d5915f08a50bf62274159
Carlos Roberto Vilela Júnior <CIANDT\carlosjr@lnb035455cps.ciandt.global>
2022-07-08 16:00:56 -0300

    starting project

---
+++ receitas_config/urls.py
@@ -0,0 +1,8 @@
+from xml.etree.ElementInclude import include
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('receitas_app.urls')),
+    path('admin/', admin.site.urls),
+]
