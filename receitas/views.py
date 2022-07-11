06c36163025a687d257430ee83099fda36c1a31e
Carlos Roberto Vilela Júnior <CIANDT\carlosjr@lnb035455cps.ciandt.global>
2022-07-11 14:17:40 -0300

    added receitas dinamicas com dados mocados

--- receitas/views.py
+++ receitas/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html')
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sopa',
+        3: 'Sorvete',
+        4: 'Bolo'
+    }
+    dados = {
+        'nomes_receitas' : receitas
+    }
+    
+    return render(request, 'index.html', dados)
 
 def receita(request):
-    return render(request,'receita.html')
+    return render(request, 'receita.html')
