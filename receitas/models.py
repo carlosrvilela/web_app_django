ac2683bacc4ee6a7109e5a1614a7d5eb77e85cc4
Carlos Roberto Vilela Júnior <CIANDT\carlosjr@lnb035455cps.ciandt.global>
2022-07-11 15:47:19 -0300

    integrando com postgres

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Receita(models.Model):
+    nome_receita = models.CharField(max_length = 250)
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
+    tempo_preparo = models.ImageField()
+    rendimento = models.CharField(max_length = 150)
+    categoria = models.CharField(max_length = 150)
+    data_receita = models.DateField(default = datetime.now, blank = True)
