6e80379f74d855539d1d74388975bf959b628eb7
Carlos Roberto Vilela Júnior <CIANDT\carlosjr@lnb035455cps.ciandt.global>
2022-07-12 14:50:47 -0300

    criado e relacionado pessoa receita

--- receitas/models.py
+++ receitas/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from datetime import datetime
 
+from pessoas.admin import Pessoa
+
 class Receita(models.Model):
     nome_receita = models.CharField(max_length = 250)
     ingredientes = models.TextField()
@@ -9,3 +11,4 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length = 150)
     categoria = models.CharField(max_length = 150)
     data_receita = models.DateField(default = datetime.now, blank = True)
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
