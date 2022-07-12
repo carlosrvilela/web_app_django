005e1a194dc69430ccdf04c3ff6f9330057991b9
Carlos Roberto Vilela Júnior <CIANDT\carlosjr@lnb035455cps.ciandt.global>
2022-07-12 14:06:46 -0300

    ajuste django admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Receita
 
-admin.site.register(Receita)
+class ReceitasList(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita', )
+    list_filter = ('categoria', )
+    list_per_page = 10
+
+admin.site.register(Receita, ReceitasList)
