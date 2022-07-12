3f10624d59c75a14fde50da96bc2c9d61e43c676
Carlos Roberto Vilela Júnior <CIANDT\carlosjr@lnb035455cps.ciandt.global>
2022-07-12 11:50:42 -0300

    added crud via django admin e finalizando recebmento de receitas via parametro

--- receitas/models.py
+++ receitas/models.py
@@ -5,7 +5,7 @@ class Receita(models.Model):
     nome_receita = models.CharField(max_length = 250)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
-    tempo_preparo = models.ImageField()
+    tempo_preparo = models.IntegerField()
     rendimento = models.CharField(max_length = 150)
     categoria = models.CharField(max_length = 150)
     data_receita = models.DateField(default = datetime.now, blank = True)
--- receitas/views.py
+++ receitas/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Receita
 
 def index(request):
-    receitas = {
-        1: 'Lasanha',
-        2: 'Sopa',
-        3: 'Sorvete',
-        4: 'Bolo'
-    }
+    receitas = Receita.objects.all()
     dados = {
-        'nomes_receitas' : receitas
+        'receitas' : receitas
     }
     
     return render(request, 'index.html', dados)
 
-def receita(request):
-    return render(request, 'receita.html')
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+    receita_exibida = {
+        'receita': receita,
+    }
+    return render(request, 'receita.html', receita_exibida)
